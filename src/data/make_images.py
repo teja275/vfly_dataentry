82f75820d412f9be60b720dc5809bf13790e0ec3
teja275 <teja275@gmail.com>
2023-10-14 14:34:51 +0530

    Modified the code to add padding on the top, bottom, left and right

--- src/data/make_images.py
+++ src/data/make_images.py
@@ -1,12 +1,11 @@
 from PIL import Image
 
 
-def split_image_to_chunks(
-    input_image, num_chunks=20, num_records=2500, add_patch=False
-):
+def split_image_to_chunks(input_image, num_chunks=20, num_records=2500, hpatch_size=1, vpatch_size=1):
     width, height = input_image.size
     chunk_height = height // num_chunks
-    record_height = height // num_records
+    hpatch_height = (height // num_records) * hpatch_size
+    vpatch_width = (height // num_records) * vpatch_size
 
     image_chunks = []
 
@@ -15,23 +14,18 @@ def split_image_to_chunks(
         right = width
         upper = i * chunk_height
         lower = (i + 1) * chunk_height
-
-        if i > 0:
-            upper = upper - record_height
-
-        if add_patch:
-            # Create a white patch image
-            patch = Image.new("RGB", (width, record_height), (255, 255, 255))
-
-            # Add the white patch to the top and bottom
-            chunk = Image.new(
-                "RGB", (width, chunk_height + 2 * record_height), (255, 255, 255)
-            )
-            chunk.paste(patch, (0, 0))
-            chunk.paste(
-                input_image.crop((left, upper, right, lower)), (0, record_height)
-            )
-        else:
-            chunk = input_image.crop((left, upper, right, lower))
-        image_chunks.append(chunk)
+        # if i > 0:
+        #     upper = upper - hpatch_height
+        cropped_image = input_image.crop((left, upper, right, lower))
+        patched_image = Image.new("RGB", (width + 2 * vpatch_width, chunk_height + 2 * hpatch_height), (255, 255, 255))
+        patched_image.paste(cropped_image, (vpatch_width, hpatch_height))
+        image_chunks.append(patched_image)
     return image_chunks
+
+
+if __name__ == '__main__':
+    file_name = '/Users/aryan/Downloads/demo.gif'
+    image = Image.open(file_name)
+    image_chunks = split_image_to_chunks(image, num_chunks=1, num_records=60, hpatch_size=1, vpatch_size=1)
+    for image in image_chunks:
+        image.show()
