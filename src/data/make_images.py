4e68bcb616d883121edf43cefe93c8a6bc543dc3
teja275 <teja275@gmail.com>
2023-10-14 15:03:04 +0530

    Enhanced image padding logic

--- src/data/make_images.py
+++ src/data/make_images.py
@@ -1,7 +1,9 @@
 from PIL import Image
 
 
-def split_image_to_chunks(input_image, num_chunks=20, num_records=2500, hpatch_size=1, vpatch_size=1):
+def split_image_to_chunks(
+    input_image, num_chunks=20, num_records=2500, hpatch_size=1, vpatch_size=1
+):
     width, height = input_image.size
     chunk_height = height // num_chunks
     hpatch_height = (height // num_records) * hpatch_size
@@ -17,15 +19,21 @@ def split_image_to_chunks(input_image, num_chunks=20, num_records=2500, hpatch_s
         # if i > 0:
         #     upper = upper - hpatch_height
         cropped_image = input_image.crop((left, upper, right, lower))
-        patched_image = Image.new("RGB", (width + 2 * vpatch_width, chunk_height + 2 * hpatch_height), (255, 255, 255))
+        patched_image = Image.new(
+            "RGB",
+            (width + 2 * vpatch_width, chunk_height + 2 * hpatch_height),
+            (255, 255, 255),
+        )
         patched_image.paste(cropped_image, (vpatch_width, hpatch_height))
         image_chunks.append(patched_image)
     return image_chunks
 
 
-if __name__ == '__main__':
-    file_name = '/Users/aryan/Downloads/demo.gif'
+if __name__ == "__main__":
+    file_name = "/Users/aryan/Downloads/demo.gif"
     image = Image.open(file_name)
-    image_chunks = split_image_to_chunks(image, num_chunks=1, num_records=60, hpatch_size=1, vpatch_size=1)
+    image_chunks = split_image_to_chunks(
+        image, num_chunks=1, num_records=60, hpatch_size=1, vpatch_size=1
+    )
     for image in image_chunks:
         image.show()
