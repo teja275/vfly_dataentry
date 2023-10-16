af8879d7088e2452bec3a1784971929e6ef2c88c
teja275 <teja275@gmail.com>
2023-10-16 19:43:14 +0530

    Added API url in config file

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 # application.py
-from flask import Flask, render_template, request, send_file, Response
-from werkzeug.utils import secure_filename
-import os
+from flask import Flask, render_template, request, Response
+from config import OCR_API_URL
 from src.run_ocr_pipeline import get_excel_from_image
 
 application = Flask(__name__, template_folder="templates")
@@ -17,12 +16,10 @@ def upload_image():
         num_records = int(request.form["num_records"])
         output_filename_prefix = request.form["file_prefix"]
         if image:
-            ocr_url = "https://app.nanonets.com/api/v2/OCR/Model/25dda2f2-e3cc-4d91-96aa-ee401d370ca8/LabelFile/?async=false"
             # Call your processing script here
             zip_file = get_excel_from_image(
-                image, num_chunks, num_records, output_filename_prefix, ocr_url
+                image, num_chunks, num_records, output_filename_prefix, OCR_API_URL
             )
-
             return Response(
                 zip_file,
                 mimetype="application/zip",
@@ -30,7 +27,6 @@ def upload_image():
                     "Content-Disposition": "attachment; filename=image_chunks.zip"
                 },
             )
-
     # Return the form for GET requests
     return render_template("upload_form.html")
 
---
+++ config.py
@@ -0,0 +1,2 @@
+# config.py
+OCR_API_URL = "https://app.nanonets.com/api/v2/OCR/Model/25dda2f2-e3cc-4d91-96aa-ee401d370ca8/LabelFile/?async=false"
