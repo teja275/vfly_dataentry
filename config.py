e47a5c569d6b052574fa36b4624426daf4306e12
Bhanuteja Aryasomayajula <bhanuteja.aryasomayajula@thomsonreuters.com>
2023-11-01 21:26:11 +0530

    Update URL to reflect daddy's account

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 # config.py
-OCR_API_URL = "https://app.nanonets.com/api/v2/OCR/Model/25dda2f2-e3cc-4d91-96aa-ee401d370ca8/LabelFile/?async=false"
+# teja account
+# OCR_API_URL = "https://app.nanonets.com/api/v2/OCR/Model/25dda2f2-e3cc-4d91-96aa-ee401d370ca8/LabelFile/?async=false"
+# daddy account
+OCR_API_URL = "https://app.nanonets.com/api/v2/OCR/Model/02b43b93-6aac-43e8-b604-39ffe937db00/LabelFile/?async=false"
