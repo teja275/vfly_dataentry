4bc3c649c34378121542ca8ef8ea0571658aeb7f
Bhanuteja Aryasomayajula <bhanuteja.aryasomayajula@thomsonreuters.com>
2023-11-08 21:49:55 +0530

    Using nanonets developer keys

--- config.py
+++ config.py
@@ -3,5 +3,8 @@
 # OCR_API_URL = "https://app.nanonets.com/api/v2/OCR/Model/25dda2f2-e3cc-4d91-96aa-ee401d370ca8/LabelFile/?async=false"
 # OCR_API_KEY = "5ac06dd4-641f-11ee-85a3-3e421bfc1e1c"
 # daddy account
-OCR_API_URL = "https://app.nanonets.com/api/v2/OCR/Model/02b43b93-6aac-43e8-b604-39ffe937db00/LabelFile/?async=false"
-OCR_API_KEY = "d26e4a8a-6595-11ee-87ad-da3ed22cf84e"
+# OCR_API_URL = "https://app.nanonets.com/api/v2/OCR/Model/02b43b93-6aac-43e8-b604-39ffe937db00/LabelFile/?async=false"
+# OCR_API_KEY = "d26e4a8a-6595-11ee-87ad-da3ed22cf84e"
+# API Free Key
+OCR_API_URL = "https://app.nanonets.com/api/v2/OCR/Model/6aace346-9b80-4c02-8fd4-138c7575382c/LabelFile/?async=false"
+OCR_API_KEY = "5_no1N0KdX4Eac_e8BnxDzdsY6ycz_rk"
