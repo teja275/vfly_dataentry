25f6b745c1d8adba877c244d48649ed8a5dd15fc
Bhanuteja Aryasomayajula <bhanuteja.aryasomayajula@thomsonreuters.com>
2023-12-12 00:04:19 +0530

    black formatting

--- src/data/clean_df.py
+++ src/data/clean_df.py
@@ -6,7 +6,9 @@ def clean_states(df_table, state_replacements_all=None, state_replacements_col=N
     df_table = df_table.replace(state_replacements_all)
     if df_table.shape[1] == 23:
         state_columns = [7, 14, 19]
-        df_table.iloc[:, state_columns] = df_table.iloc[:, state_columns].replace(state_replacements_col)
+        df_table.iloc[:, state_columns] = df_table.iloc[:, state_columns].replace(
+            state_replacements_col
+        )
     return df_table
 
 
@@ -15,18 +17,27 @@ def clean_address(df_table, address_replacements=None):
         address_replacements = {" \.": "."}
     if df_table.shape[1] == 23:
         address_columns = [3, 12, 18]
-        df_table.iloc[:, address_columns] = df_table.iloc[:, address_columns].replace(address_replacements, regex=True)
+        df_table.iloc[:, address_columns] = df_table.iloc[:, address_columns].replace(
+            address_replacements, regex=True
+        )
     return df_table
 
 
 def clean_mediclaim_number(df_table):
     if df_table.shape[1] == 23:
         mediclaim_number_column = 9
-        df_table.iloc[:, mediclaim_number_column] = df_table.iloc[:, mediclaim_number_column].apply(lambda x: x if not str(x).endswith("4") else str(x)[:-1] + "A")
+        df_table.iloc[:, mediclaim_number_column] = df_table.iloc[
+            :, mediclaim_number_column
+        ].apply(lambda x: x if not str(x).endswith("4") else str(x)[:-1] + "A")
     return df_table
 
 
-def clean_df(df_table, state_replacements_all=None, state_replacements_col=None, address_replacements=None):
+def clean_df(
+    df_table,
+    state_replacements_all=None,
+    state_replacements_col=None,
+    address_replacements=None,
+):
     df_table = clean_states(df_table, state_replacements_all, state_replacements_col)
     df_table = clean_address(df_table, address_replacements)
     df_table = clean_mediclaim_number(df_table)
